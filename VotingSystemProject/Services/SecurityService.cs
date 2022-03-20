92f7b35e34f4fd74f83d61850d74718577f74c38
Emin Ölcer <Eminolcer@gmail.com>
2022-03-20 02:47:55 +0100

    Added Database Connection

--- VotingSystemProject/Services/SecurityService.cs
+++ VotingSystemProject/Services/SecurityService.cs
@@ -4,17 +4,15 @@ namespace VotingSystemProject.Services
 {
     public class SecurityService
     {
-        List<UserModel> knownUsers = new List<UserModel>();
+        UsersDAO usersDAO = new UsersDAO();
 
         public SecurityService()
         {
-            knownUsers.Add(new UserModel { Id = 0, Name = "Emin", Password = "123emin" });
-            knownUsers.Add(new UserModel { Id = 1, Name = "Saif", Password = "123saif" });
-            knownUsers.Add(new UserModel { Id = 2, Name = "Alec", Password = "123alec" });
+         
         }
         public bool IsValid(UserModel user)
         {
-            return knownUsers.Any(x => x.Name == user.Name && x.Password == user.Password);
+            return usersDAO.FindUserByNameAndPassword(user);
         }
     }
 }
---
+++ VotingSystemProject/Services/UsersDAO.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+using VotingSystemProject.Models;
+
+namespace VotingSystemProject.Services
+{
+    public class UsersDAO
+    {
+
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VotingSystemProjectDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public bool FindUserByNameAndPassword(UserModel user)
+        {
+            bool success = false;
+
+            string sqlStatement = "SELECT * FROM dbo.Users WHERE name = @name AND password = @password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.Add("@name", System.Data.SqlDbType.VarChar, 50).Value = user.Name;
+
+                command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                }catch (Exception f)
+                {
+                    Console.WriteLine(f.Message);
+
+                }
+
+            }
+            return success;
+
+
+        }
+    }
+}
