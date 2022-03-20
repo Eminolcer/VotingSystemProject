4ad2dc19913e8975ea8ffc62dce9829a87a4b979
Emin Ölcer <Eminolcer@gmail.com>
2022-03-20 01:46:50 +0100

    Added Login Test

---
+++ VotingSystemProject/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VotingSystemProject.Models;
+using VotingSystemProject.Services;
+
+namespace VotingSystemProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.IsValid(userModel))
+            { 
+                return View("LoginSuccess", userModel);
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+
+        }
+    }
+}
---
+++ VotingSystemProject/Services/SecurityService.cs
@@ -0,0 +1,20 @@
+﻿using VotingSystemProject.Models;
+
+namespace VotingSystemProject.Services
+{
+    public class SecurityService
+    {
+        List<UserModel> knownUsers = new List<UserModel>();
+
+        public SecurityService()
+        {
+            knownUsers.Add(new UserModel { Id = 0, Name = "Emin", Password = "123emin" });
+            knownUsers.Add(new UserModel { Id = 1, Name = "Saif", Password = "123saif" });
+            knownUsers.Add(new UserModel { Id = 2, Name = "Alec", Password = "123alec" });
+        }
+        public bool IsValid(UserModel user)
+        {
+            return knownUsers.Any(x => x.Name == user.Name && x.Password == user.Password);
+        }
+    }
+}
